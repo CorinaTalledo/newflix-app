c1c3a0c6fb1adfe9f4ff41aacea514c8032912c2
CorinaTalledo <corina.talledo@gmail.com>
2024-05-11 19:08:12 -0300

    basic layout

---
+++ src/views/ContainMovies.jsx
@@ -0,0 +1,29 @@
+import CardMovie from "../components/CardMovie";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import Box from "@mui/material/Box";
+
+export default function ContainMovies() {
+  return (
+    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+      <Typography variant="h2" gutterBottom>
+        Ultimos Lanzamientos oooo Populares
+      </Typography>
+
+      <CardMovie />
+
+      <Pagination
+      /* page="1"
+        count={10}
+        renderItem={(item) => (
+          <PaginationItem
+            component={Link}
+            to={`/inbox${item.page === 1 ? "" : `?page=${item.page}`}`}
+            {...item}
+          />
+        )} */
+      />
+    </Box>
+  );
+}
