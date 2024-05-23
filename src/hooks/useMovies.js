1ba8d6712062ec39a8fe2492620df5355967440e
CorinaTalledo <corina.talledo@gmail.com>
2024-05-22 21:21:36 -0300

    detail-movie

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function useMovies() {
   const [popularMovies, setPopularMovies] = useState([]);
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [initialCarrusel, setInitialCarrusel] = useState([]);
+  const [oneMovie, setOneMovie] = useState({});
+  const [trailer, setTrailer] = useState();
+
+  const { idMovie } = useParams();
 
   async function getPopularMovies() {
     try {
@@ -79,6 +84,58 @@ export default function useMovies() {
     }
   }
 
+  async function getOneMovie() {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${idMovie}`,
+        {
+          params: {
+            include_adult: false,
+            include_video: false,
+            language: "en-US",
+            /*             append_to_response: "videos",
+             */
+          },
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzkxNmIwNTA0Zjc0YjlmMjQ2NzFiYjRmMGZmMjhkMyIsInN1YiI6IjY2M2ZmMjZlNTNhMmU1Yzc5MzBiMzg3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4AIbiaaCpVKgqLqrtHpZqhBa8ZXfWWgAMIoqD5YpL8E",
+          },
+        }
+      );
+
+      setOneMovie(response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  async function getTrailer() {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${idMovie}/videos`,
+        {
+          params: {
+            include_adult: false,
+            include_video: false,
+            language: "en-US",
+          },
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzkxNmIwNTA0Zjc0YjlmMjQ2NzFiYjRmMGZmMjhkMyIsInN1YiI6IjY2M2ZmMjZlNTNhMmU1Yzc5MzBiMzg3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4AIbiaaCpVKgqLqrtHpZqhBa8ZXfWWgAMIoqD5YpL8E",
+          },
+        }
+      );
+
+      setTrailer(
+        response.data.results.find(({ name }) => name === "Official Trailer")
+      );
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return {
     popularMovies,
     getPopularMovies,
@@ -86,5 +143,9 @@ export default function useMovies() {
     getTopRatedMovies,
     initialCarrusel,
     getPopularCarrusel,
+    oneMovie,
+    getOneMovie,
+    trailer,
+    getTrailer,
   };
 }
--- src/views/DetailMovie.jsx
+++ src/views/DetailMovie.jsx
@@ -2,69 +2,167 @@ import {
   Card,
   CardMedia,
   Typography,
+  CardContent,
   Box,
-  CardActionArea,
   List,
   ListItem,
-  ListItemIcon,
   ListItemText,
+  Button,
+  Modal,
 } from "@mui/material";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 
-/* import { useParams } from "react-router-dom";
-import { useState } from "react"; */
+import { useEffect, useState } from "react";
+
+import useMovies from "../hooks/useMovies";
 
 export default function DetailMovie() {
-  /*   const { idMovie } = useParams();
+  const { oneMovie, getOneMovie, trailer, getTrailer } = useMovies();
 
-  const [movieDetail, setMovieDetail] = useState();
+  useEffect(() => {
+    getOneMovie();
+  }, []);
 
-  console.log(idMovie); */
+  useEffect(() => {
+    getTrailer();
+  }, []);
+
+  console.log(trailer);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-        </CardActionArea>
+    <Box
+      component="section"
+      height="79vh"
+      width="100vw"
+      sx={{
+        display: "flex",
+        /* alignItems: "center", */
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)), url(https://image.tmdb.org/t/p/original/${oneMovie.backdrop_path})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Card
+        sx={{
+          margin: "auto",
+          width: "70%",
+          height: "85%",
+          backgroundColor: "transparent",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{
+            width: "30%",
+            height: "auto",
+            alignSelf: "center",
+          }}
+          image={`https://image.tmdb.org/t/p/original/${oneMovie.poster_path}`}
+          alt={oneMovie.title}
+        />
+
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            color: "white",
+            width: "60%",
+            border: "3px solid white",
+          }}
+        >
+          <Box
+            sx={{
+              border: "3px solid white",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              /*               height: "80%",
+               */
+            }}
+          >
+            <Box
+              sx={{
+                border: "3px solid red",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+                height: "80%",
+              }}
+            >
+              <Box sx={{ border: "3px solid red", width: "70%" }}>
+                <Typography variant="h4" /* component="div" */>
+                  {oneMovie.title}
+                </Typography>
+
+                <Typography variant="subtitle1" gutterBottom component="span">
+                  {oneMovie.release_date &&
+                    oneMovie.release_date.toString().slice(0, 4)}
+                </Typography>
+              </Box>
+
+              <Button
+                onClick={handleOpen}
+                startIcon={<PlayCircleOutlineIcon />}
+                size="small"
+              >
+                Watch Trailer
+              </Button>
+            </Box>
+            <Typography variant="body1">{oneMovie.overview}</Typography>
+          </Box>
+
+          <Box>
+            <Typography variant="subtitle1" gutterBottom>
+              Genres
+            </Typography>
+
+            <List>
+              {oneMovie.genres &&
+                oneMovie.genres.map((genre) => (
+                  <ListItem disableGutters key={genre.id}>
+                    <ArrowRightIcon fontSize="large" />
+                    <ListItemText primary={genre.name} />
+                  </ListItem>
+                ))}
+            </List>
+          </Box>
+        </CardContent>
       </Card>
 
-      <Box>
-        <Typography variant="h3" gutterBottom>
-          TITULO
-        </Typography>
-
-        <Typography variant="subtitle1" gutterBottom>
-          General
-        </Typography>
-
-        <Typography variant="body2" gutterBottom>
-          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
-
-        <Typography variant="subtitle1" gutterBottom>
-          Géneros
-        </Typography>
-
-        <List /* dense={dense} */>
-          {/* {generate( */}
-          <ListItem>
-            <ListItemIcon>X</ListItemIcon>
-            <ListItemText
-              primary="Single-line item"
-              /* secondary={secondary ? "Secondary text" : null} */
-            />
-          </ListItem>
-          {/* )} */}
-        </List>
-      </Box>
-    </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "black",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <iframe
+          width="80%"
+          height="80%"
+          src={`https://www.youtube.com/embed/${trailer?.key}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </Modal>
+    </Box>
   );
 }
