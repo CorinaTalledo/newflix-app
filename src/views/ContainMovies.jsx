98bbd929308aa7c7886223d082ab82727725a298
CorinaTalledo <corina.talledo@gmail.com>
2024-05-24 22:26:03 -0300

    releases and popular + optimization of api calls

--- src/views/ContainMovies.jsx
+++ src/views/ContainMovies.jsx
@@ -1,28 +1,68 @@
 import CardMovie from "../components/CardMovie";
-import Typography from "@mui/material/Typography";
-import Pagination from "@mui/material/Pagination";
-import PaginationItem from "@mui/material/PaginationItem";
-import Box from "@mui/material/Box";
+
+import { Typography, Pagination, Box, Stack } from "@mui/material";
+
+import { useEffect } from "react";
+import useMovies from "../hooks/useMovies";
+import { useParams } from "react-router-dom";
 
 export default function ContainMovies() {
+  const { category } = useParams();
+
+  const {
+    movies,
+    getMoviesByCategory,
+    totalPages,
+    page,
+    handlePagination,
+    resetPage,
+  } = useMovies();
+
+  useEffect(() => {
+    getMoviesByCategory(category);
+  }, [category, page]);
+
+  useEffect(() => {
+    resetPage();
+  }, [category]);
+  console.log(category);
+
   return (
-    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+    <Box
+      component="section"
+      sx={{
+        p: 2,
+        border: "1px dashed grey",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        alignContent: "center",
+      }}
+    >
       <Typography variant="h2" gutterBottom>
-        Ultimos Lanzamientos oooo Populares
+        {category === "popular" ? "Popular Movies" : "Last Releases"}
       </Typography>
 
-      {/* <CardMovie /> */}
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        alignContent="center"
+        flexWrap="wrap"
+        gap={5}
+        sx={{ width: "90%", border: "1px dashed grey" }}
+      >
+        {movies.map((movie) => (
+          <CardMovie key={movie.id} movie={movie} />
+        ))}
+      </Stack>
 
       <Pagination
-      /* page="1"
-        count={10}
-        renderItem={(item) => (
-          <PaginationItem
-            component={Link}
-            to={`/inbox${item.page === 1 ? "" : `?page=${item.page}`}`}
-            {...item}
-          />
-        )} */
+        count={totalPages} // Le dice a MUI cuantas paginas debe habilitar
+        page={page}
+        onChange={handlePagination}
+        variant="outlined"
+        shape="rounded"
       />
     </Box>
   );
