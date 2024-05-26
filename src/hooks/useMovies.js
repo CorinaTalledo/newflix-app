5cb5c78d7f5e0101ca33f764d03fdb1f55cbbd64
CorinaTalledo <corina.talledo@gmail.com>
2024-05-26 17:49:11 -0300

    search view and functionalities

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -3,14 +3,12 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export default function useMovies() {
-  const [popularMovies, setPopularMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [initialCarrusel, setInitialCarrusel] = useState([]);
   const [oneMovie, setOneMovie] = useState({});
   const [trailer, setTrailer] = useState();
-  // const [releases, setReleases] = useState([]);
-
-  const [movies, setMovies] = useState([]);
 
   const [totalPages, setTotalPages] = useState(1);
   const [page, setPage] = useState(1);
@@ -144,6 +142,31 @@ export default function useMovies() {
     }
   }
 
+  async function getSearchedMovie(inputValue) {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?query=${inputValue}`,
+        {
+          params: {
+            include_adult: false,
+            include_video: false,
+            language: "en-US",
+          },
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzkxNmIwNTA0Zjc0YjlmMjQ2NzFiYjRmMGZmMjhkMyIsInN1YiI6IjY2M2ZmMjZlNTNhMmU1Yzc5MzBiMzg3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4AIbiaaCpVKgqLqrtHpZqhBa8ZXfWWgAMIoqD5YpL8E",
+          },
+        }
+      );
+
+      setMovies(response.data.results);
+      setTotalPages(response.data.total_pages);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   const handlePagination = (event, pagenumber) => {
     setPage(pagenumber);
   };
@@ -164,6 +187,7 @@ export default function useMovies() {
 
     movies,
     getMoviesByCategory,
+    getSearchedMovie,
     totalPages,
     page,
     handlePagination,
